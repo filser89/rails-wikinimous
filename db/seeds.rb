793de6706573ed34df73aa19ed791288d5a1c62c
Sergey Filippov <sergey.filippov.89@inbox.ru>
2020-07-28 18:30:53 +0800

    articles website done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all
+
+10.times do |variable|
+  Article.create(title: Faker::Sports::Football.team, content: Faker::Quote.matz)
+end
